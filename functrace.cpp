dd5f3427e86fcfe81adeb31701161e91871e26cf
Andrea Sindoni <andrea.sindoni@gmail.com>
2019-01-05 23:53:03 +0100

    Update functrace.cpp

--- functrace.cpp
+++ functrace.cpp
@@ -18,7 +18,7 @@
 static void *mod_lock;
 static void *wrap_lock;
 static file_t fd;
-static size_t towrap;
+static size_t towrap = 0;
 static drsym_info_t *syminfo;
 
 typedef struct option_t {
@@ -37,9 +37,9 @@ static option_t options;
 
 struct funct {
     const char *name_function;
-    size_t start_addr;
-    size_t end_addr;
-    size_t pc;
+    app_pc start_addr;
+    app_pc end_addr;
+    app_pc pc;
 };
 
 static void wrap_pre(void *wrapcxt, OUT void **user_data) {
@@ -47,9 +47,9 @@ static void wrap_pre(void *wrapcxt, OUT void **user_data) {
 
     dr_mutex_lock(wrap_lock);
     for (i = 0; i < options.wrap_function_args; i++) {
-        uint64 arg = (uint64) drwrap_get_arg(wrapcxt, i);
+        app_pc arg = (app_pc) drwrap_get_arg(wrapcxt, i);
         DR_ASSERT(arg);
-        dr_fprintf(fd, "[ARG] Function: %s Arg %d: 0x%llx \n\n", options.wrap_function_name, i, arg);
+        dr_fprintf(fd, "[ARG] Function: %s Arg %d: " PFX " \n\n", options.wrap_function_name, i, arg);
     }
     dr_mutex_unlock(wrap_lock);
 }
@@ -57,19 +57,36 @@ static void wrap_pre(void *wrapcxt, OUT void **user_data) {
 static void wrap_post(void *wrapcxt, void *user_data) {
     dr_mutex_lock(wrap_lock);
     
-    uint64 ret = (uint64) drwrap_get_retval(wrapcxt);
+    app_pc ret = (app_pc) drwrap_get_retval(wrapcxt);
     
-    dr_fprintf(fd, "[RET] Function: %s ret_value: 0x%llx \n\n", options.wrap_function_name, ret);
+    dr_fprintf(fd, "[RET] Function: %s ret_value: " PFX " \n\n", options.wrap_function_name, ret);
     
     dr_mutex_unlock(wrap_lock);
 }
 
+static void iterate_imports(const module_data_t *mod)
+{
+    const char *mod_name = dr_module_preferred_name(mod);
+
+    dr_symbol_import_iterator_t *imp_iter =
+        dr_symbol_import_iterator_start(mod->handle, NULL);
+    while (dr_symbol_import_iterator_hasnext(imp_iter)) {
+        dr_symbol_import_t *sym = dr_symbol_import_iterator_next(imp_iter);
+        dr_fprintf(fd, "Name %s\n", sym->name);
+    }
+    dr_symbol_import_iterator_stop(imp_iter);
+}
+
 static bool enumerate_sym(const char *name, size_t modoffs, void *data) {
-    if (*name!=0) {
-        if (options.verbose)
+    if (*name != 0) {
+        if (options.verbose){
             dr_fprintf(fd, "Offset: %x Name: %s\n", modoffs, name);
-        if (!strcmp(name, options.wrap_function_name))
+        }
+        if (!strcmp(name, options.wrap_function_name)) {
             towrap = modoffs;
+        } else {
+            towrap = 0;
+        }
     }
     return true;
 }
@@ -79,7 +96,7 @@ static void event_module_load(void *drcontext, const module_data_t *mod, bool lo
     module_data_t *data = dr_get_main_module();
 
     if (data == NULL) {
-        dr_fprintf(STDERR, "No main module found! \n");
+        dr_fprintf(fd, "[ERR] No main module found! \n");
         return;
     }
     
@@ -98,32 +115,38 @@ static void event_module_load(void *drcontext, const module_data_t *mod, bool lo
     }
 
     drsym_error_t symr;
+    
+    if (options.verbose) {
+        dr_fprintf(fd, "IMPORTS: \n");
+        iterate_imports(mod);
+        dr_fprintf(fd, "EXPORTS: \n");
+    }
+
     symr = drsym_enumerate_symbols(mod->full_path, enumerate_sym, NULL, DRSYM_DEFAULT_FLAGS);
-    DR_ASSERT(symr == DRSYM_SUCCESS); 
+    if (symr != DRSYM_SUCCESS && options.verbose)
+        dr_fprintf(fd, "[ERR] search / enum error %d\n", symr);
 
     if (options.wrap_function) {
         bool wrapped = false;
-        towrap = (size_t)mod_base + towrap;
+        app_pc to_wrap = mod_base + towrap;
         
         if (towrap != 0) {
-            wrapped = drwrap_wrap((app_pc)towrap, wrap_pre, wrap_post);
-        }
-
-        if (wrapped) {
-            dr_fprintf(STDERR, "WRAPPED function: %s \n", options.wrap_function_name);
-        } else {
-            dr_fprintf(STDERR, "FAILED to wrap %s \n", options.wrap_function_name);
+            wrapped = drwrap_wrap(to_wrap, wrap_pre, wrap_post);
+            DR_ASSERT(wrapped);
         }
     }
 
     dr_free_module_data(data);
 }
 
-static drsym_info_t* drsym_obj() {
+static drsym_info_t* drsym_obj(const char *ath) {
     drsym_info_t* drsym_o;
     drsym_o = (drsym_info_t*)malloc(sizeof(drsym_info_t));
     drsym_o->struct_size = sizeof(drsym_info_t);
-    drsym_o->debug_kind = DRSYM_SYMBOLS;    
+    drsym_debug_kind_t kind;
+    drsym_error_t symres = drsym_get_module_debug_kind(path, &kind);
+    if (symres == DRSYM_SUCCESS)
+        drsym_o->debug_kind = kind;    
     drsym_o->name_size = LEN;
     drsym_o->file_size = LEN;
     drsym_o->file=(char*)malloc(LEN);
@@ -131,7 +154,7 @@ static drsym_info_t* drsym_obj() {
     return drsym_o;
 }
  
-static void free_drsmy_obj(drsym_info_t * drsym_o) {
+static void free_drsmy_obj(drsym_info_t *drsym_o) {
     if (drsym_o->file != NULL) 
         free(drsym_o->file);
     if (drsym_o->name != NULL) 
@@ -141,24 +164,27 @@ static void free_drsmy_obj(drsym_info_t * drsym_o) {
 
 static dr_emit_flags_t event_bb_analysis(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating, OUT void **user_data) {
     instr_t *instr;
+    instr_t *last;
     module_data_t *data;
     module_data_t *mod;
     struct funct functions;
-
+    
     drsym_error_t symres;
     
-    instr  = instrlist_first_app(bb);
+    instr = instrlist_first_app(bb);
+    last = instrlist_last_app(bb);
 
     if (instr == NULL)
         return DR_EMIT_DEFAULT;
 
     app_pc pc = instr_get_app_pc(instr);
+    app_pc last_bb = instr_get_app_pc(last);
 
     mod = dr_lookup_module(pc);
     
     if (mod == NULL)
         return DR_EMIT_DEFAULT;
-        
+            
     app_pc mod_base = mod->start;
 
     data = dr_get_main_module();
@@ -168,30 +194,30 @@ static dr_emit_flags_t event_bb_analysis(void *drcontext, void *tag, instrlist_t
 
     if (mod_base != data->start)
         return DR_EMIT_DEFAULT;
-    
+
     dr_mutex_lock(mod_lock);
 
-    syminfo = drsym_obj();
+    syminfo = drsym_obj(mod->full_path);
 
-    size_t offset = (size_t)pc - (size_t)mod_base;
+    size_t offset = pc - mod_base;
     syminfo->start_offs = 0;
     syminfo->end_offs = 0;
     symres = drsym_lookup_address(mod->full_path, offset, syminfo, DRSYM_DEMANGLE);
-
+        
+    functions.pc = pc;
     if (symres == DRSYM_SUCCESS || symres == DRSYM_ERROR_LINE_NOT_AVAILABLE) {
         functions.name_function = syminfo->name;
-        functions.start_addr = syminfo->start_offs + (size_t)mod_base;
-        functions.end_addr = syminfo->end_offs + (size_t)mod_base;
-        functions.pc = (size_t)pc;
-        dr_fprintf(fd, "[ADDR] Start address: 0x%x End Address: 0x%x PC: 0x%x Function: %s\n", functions.start_addr, functions.end_addr, functions.pc, functions.name_function);
+        functions.start_addr = syminfo->start_offs + mod_base;
+        functions.end_addr = syminfo->end_offs + mod_base;
+        dr_fprintf(fd, "[ADDR] Start address: 0x%x End Address: 0x%x PC: " PFX " Function: %s\n", functions.start_addr, functions.end_addr, functions.pc, functions.name_function);
         if (options.disassembly && !options.disassembly_function)
             instrlist_disassemble(drcontext, (app_pc)tag, bb, fd);
         if (!options.disassembly && options.disassembly_function && !strcmp(options.function_name, functions.name_function)){
             instrlist_disassemble(drcontext, (app_pc)tag, bb, fd);
         }
     } else {
-        functions.pc = (size_t)pc;
-        dr_fprintf(fd, "[NOSYM] PC: 0x%x Function: NoSym\n", functions.pc);
+        app_pc first_bb = pc;
+        dr_fprintf(fd, "[NOSYM] PC: " PFX " BB Start address: " PFX " BB End Address: " PFX " \n", pc, first_bb, last_bb);
         if (options.disassembly && !options.disassembly_function)
             instrlist_disassemble(drcontext, (app_pc)tag, bb, fd);
     }
